adbaca7b99afded3294d08f3d323a990a28d90ce
jwok938 <jwok938@gmail.com>
2023-11-09 13:18:40 +0800

    pnr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 export default function App() {
   return (
-    <h1 className='text-red-500' >App</h1>
+    <BrowserRouter>
+      <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/about" element={<About />} />
+              
+      </Routes>
+   </BrowserRouter>
   )
 }
