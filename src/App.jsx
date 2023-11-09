2ff7d0ee4c0bf053edf220577f584e91139e1026
jwok938 <jwok938@gmail.com>
2023-11-09 21:57:09 +0800

    hnL

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/signin" element={<Signin />} />
